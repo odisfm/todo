f7434ab26ee2348f2812241c86c08905310f4bb4
odisfm <102568741+odisfm@users.noreply.github.com>
2024-07-07 16:33:36 +1000

    Add webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { fileURLToPath } = require('url');
+
+module.exports = {
+  mode: 'development',  
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Development',
+        filename: 'index.html',
+        inject: 'body',
+    })
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(csv|tsv)$/i,
+            use: ['csv-loader'],   
+        },
+        {
+            test: /\.xml$/i,
+            use: ['xml-loader'],
+        },
+    ],
+  },
+};
